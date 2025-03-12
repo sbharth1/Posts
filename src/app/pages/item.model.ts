673b99b6b814bd07b7f1415830ae00ed7dc1ee18
siddharth <baghelsiddharth942@gmail.com>
2025-03-12 16:29:20 +0530

    dyamic cards for apis data

---
+++ src/app/pages/item.model.ts
@@ -0,0 +1,7 @@
+export class Item {
+    constructor(
+      public item: number,
+      public content: string
+    ) {}
+  }
+  
--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../item.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss'
+  styleUrl: './posts.component.scss',
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit{
+  items:Item[] = [
+    { item: 1, content: 'fake text' },
+    { item: 2, content: 'fake text' },
+    { item: 3, content: 'fake text' },
+    { item: 4, content: 'fake text' },
+    { item: 5, content: 'fake text' },
+  ];
+
+  allItems:Item[] = [];
+
+  ngOnInit():void{
+    this.allItems = this.items.map((item)=> ({...item}))
+  }
 
 }
