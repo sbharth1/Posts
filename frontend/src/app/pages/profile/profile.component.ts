04e41a2926fe46cc0ade8643188260568646b388
siddharth <baghelsiddharth942@gmail.com>
2025-03-28 15:37:32 +0530

    add some style in profile component

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
 
+export class ProfileComponent implements OnInit {
+  user = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'john.doe@example.com',
+    bio: 'A passionate web developer with experience in Angular, React, and Node.js.',
+    profileImage: 'https://imgs.search.brave.com/48WZ-vtUsJQmBsNQiFHFwjaVoYUBa1JiCSRx1uXhL0c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNS9Qcm9m/aWxlLVBORy1DbGlw/YXJ0LnBuZw' 
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
