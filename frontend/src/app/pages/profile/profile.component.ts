41203d38e93dc58aeeb77eb8c7bfd38b7b892b6f
siddharth <baghelsiddharth942@gmail.com>
2025-03-28 16:09:12 +0530

    show single user posts

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -13,8 +13,13 @@ export class ProfileComponent implements OnInit {
     username: 'johndoe',
     email: 'john.doe@example.com',
     bio: 'A passionate web developer with experience in Angular, React, and Node.js.',
-    profileImage: 'https://imgs.search.brave.com/48WZ-vtUsJQmBsNQiFHFwjaVoYUBa1JiCSRx1uXhL0c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNS9Qcm9m/aWxlLVBORy1DbGlw/YXJ0LnBuZw' 
+    profileImage: 'https://imgs.search.brave.com/48WZ-vtUsJQmBsNQiFHFwjaVoYUBa1JiCSRx1uXhL0c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNS9Qcm9m/aWxlLVBORy1DbGlw/YXJ0LnBuZw' ,
+    title:"this is title",
+    content:"this is content",
+    imageUrl:"https://imgs.search.brave.com/IZjbwlMrIp_xIfdBjK17aeTY3knLcsJEx8x83NjNI8k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNS9Qcm9m/aWxlLUZlbWFsZS1Q/TkcucG5n"
   };
+
+ 
 
   constructor() { }
 
