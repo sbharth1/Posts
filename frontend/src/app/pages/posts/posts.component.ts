3009e15f146a4f26f41a3b3b50ea6c9fca10f94f
Siddharth <165468666+sbharth1@users.noreply.github.com>
2025-03-28 12:47:17 +0530

    create route profile to user see all own posts and delete

--- frontend/src/app/pages/posts/posts.component.ts
+++ frontend/src/app/pages/posts/posts.component.ts
@@ -53,12 +53,10 @@ export class PostsComponent implements OnInit {
 
   closeResult: WritableSignal<string> = signal('');
 
-  allItems:Item[]  = [];
-
+  allItems: Item[] = [];
 
   ngOnInit(): void {
-    
-    this.getAllPosts();  // to get all posts 
+    this.getAllPosts(); // to get all posts
 
     this.modalForm = this.fb.group({
       description: ['', [Validators.required]],
@@ -69,7 +67,6 @@ export class PostsComponent implements OnInit {
       this.token = localStorage.getItem('token');
     }
   }
-
 
   // get all posts--------------------------------------
 
@@ -84,93 +81,96 @@ export class PostsComponent implements OnInit {
       )
       .subscribe((res: any) => {
         if (res) {
-        this.allItems =res;
+          this.allItems = res;
         } else {
-          console.log('Error: No response data');        }  
+          console.log('Error: No response data');
+        }
       });
   }
-  
+
   // modal open & close code ----------------------------------------
-  
+
   open(content: TemplateRef<any>) {
     this.modalService
-    .open(content, { ariaLabelledBy: 'modal-basic-title' })
-    .result.then(
-      (result: any) => {
-        this.closeResult.set(`Closed with: ${result}`);
-      },
-      (reason: any) => {
-        this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
-      }
-    );
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result: any) => {
+          this.closeResult.set(`Closed with: ${result}`);
+        },
+        (reason: any) => {
+          this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+        }
+      );
   }
-  
+
   private getDismissReason(reason: any): string {
     switch (reason) {
       case ModalDismissReasons.ESC:
         return 'by pressing ESC';
-        case ModalDismissReasons.BACKDROP_CLICK:
-          return 'by clicking on a backdrop';
-          default:
-            return `with: ${reason}`;
-          }
-        }
-        
-        
-        //  modal add post code ----------------------------------------
-        
-        onFileChange(event: any) {
-          const file = event.target.files[0];
-          if (file) {
-            console.log(file, '---- image file');
-            this.modalForm.patchValue({
-              image: file,
-            });
-          }
-        }
-        
+      case ModalDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on a backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
 
-        onAddPost(): void {
-          if (this.modalForm.valid) {
-            const formData = new FormData();
-            formData.append('description', this.modalForm.get('description')?.value);
-            formData.append('image', this.modalForm.get('image')?.value);
-            const headers =  { Authorization: `Bearer ${this.token}` };
-            this.http
-              .post('http://localhost:3700/posts', formData,{headers})
-              .pipe(
-                catchError((error) => {
-                  this.toastr.error('Post failed pls login user...');
-                  console.error('Post error', error);
-                  return error;
-                })
-              )
-              .subscribe((res: any) => {
-                if (res) {
-                  this.toastr.success("Post created successfully")
-                  this.getAllPosts();
-                } else {
-                  console.log('Error: No response data');
-                }
-              });
-        
-            this.modalForm.reset();
-            this.modalService.dismissAll();
+  //  modal add post code ----------------------------------------
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      console.log(file, '---- image file');
+      this.modalForm.patchValue({
+        image: file,
+      });
+    }
+  }
+
+  onAddPost(): void {
+    if (this.modalForm.valid) {
+      const formData = new FormData();
+      formData.append('description', this.modalForm.get('description')?.value);
+      formData.append('image', this.modalForm.get('image')?.value);
+      const headers = { Authorization: `Bearer ${this.token}` };
+      this.http
+        .post('http://localhost:3700/posts', formData, { headers })
+        .pipe(
+          catchError((error) => {
+            this.toastr.error('Post failed pls login user...');
+            console.error('Post error', error);
+            return error;
+          })
+        )
+        .subscribe((res: any) => {
+          if (res) {
+            this.toastr.success('Post created successfully');
+            this.getAllPosts();
           } else {
-            this.toastr.error('Both fields are required..');
+            console.log('Error: No response data');
           }
-        }
-          
-          
-          
-          // end modal code ----------------------------------------
-          
-          
-          
-          // navigation code----------------------------------------
+        });
+
+      this.modalForm.reset();
+      this.modalService.dismissAll();
+    } else {
+      this.toastr.error('Both fields are required..');
+    }
+  }
+
+  // end modal code ----------------------------------------
+
+
+  // delete user ------------------------------------------
+
+
+  // navigation code----------------------------------------
 
   navigateToSignIn() {
     this.router.navigate(['login']);
+  }
+
+  navigateProfile(){
+    this.router.navigate(['profile'])
   }
 
   // LogOut----------------------------------------
@@ -179,5 +179,4 @@ export class PostsComponent implements OnInit {
     localStorage.removeItem('token');
     this.token = null;
   }
-
 }
