876d66db21b2b3b07188a858ada23c5afce28681
siddharth <baghelsiddharth942@gmail.com>
2025-04-01 10:35:52 +0530

    delete selector.ts file

--- frontend/src/app/features/user/state/user.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { UserState } from './user.reducer';
-
-export const selectUserState = createFeatureSelector<UserState>('user');
-
-export const selectAllUsers = createSelector(selectUserState, (state: UserState) => state.users);
-export const selectUserById = (id: string) =>
-  createSelector(selectUserState, (state: UserState) => state.users.find(user => user.id === id));
-export const selectLoading = createSelector(selectUserState, (state: UserState) => state.loading);
